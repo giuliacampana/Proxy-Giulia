b864efa1c7d3cb02a6732ad34eb5a8d5fbd80286
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-06-15 21:32:19 -0700

    Update routing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -11,3 +12,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
+app.get('/overview/:id', (req, res) => {
+	const id = req.params.id;
+  axios
+    .get(`http://localhost:3002/api/overview/${id}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(`Cannot get /overview/:id, ${err}`));
+})
+
+app.get('/hostels', (req, res) => {
+  axios
+    .get('http://localhost:3002/api/hostels')
+    .then(response => res.send(response.data))
+    .catch(err => res.send(`Cannot get /hostels ${err}`));
+})
+
+app.get('/hostels/:id/info', (req, res) => {
+	const id = req.params.id;
+  axios
+    .get(`http://localhost:3002/api/hostels/${id}/info`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(`Cannot get /hostels ${err}`));
+})
