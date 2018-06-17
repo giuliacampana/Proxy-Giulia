a800245e7536a5e26e78fa699d324f350a8fc2f2
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-06-17 14:01:33 -0700

    Add deployed microservices ports

--- server.js
+++ server.js
@@ -1,37 +1,97 @@
-const axios = require('axios');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
 
-app.get('/overview/:id', (req, res) => {
-	const id = req.params.id;
-  axios
-    .get(`http://localhost:3002/api/overview/${id}`)
-    .then(response => res.send(response.data))
-    .catch(err => res.send(`Cannot get /overview/:id, ${err}`));
-})
 
-app.get('/hostels', (req, res) => {
+//HEADER
+app.get('/api/locations/hostels/:id/info', (req, res) => {
+  const { id } = req.params;
   axios
-    .get('http://localhost:3002/api/hostels')
+    .get(`http://18.191.246.21/api/locations/hostels/${id}/info`)
     .then(response => res.send(response.data))
-    .catch(err => res.send(`Cannot get /hostels ${err}`));
-})
+    .catch(err => res.status(404).json({ Error: err }));
+});
+  
+app.get('/api/locations/:id/info', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://18.191.246.21/api/locations/${id}/info`)
+    .then(response => res.send(response.data))
+    .catch(err => res.json({ message: 'Cannot GET /api/locations/:id/info' }));
+});
+  
+app.get('/api/locations/hostels', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://18.191.246.21/api/locations/${id}/hostels`)
+    .then(response => res.send(response.data))
+    .catch(err => res.json({ message: 'Cannot GET /locations/hostels' }));
+});
 
-app.get('/hostels/:id/info', (req, res) => {
-	const id = req.params.id;
+// OVERVIEW
+app.get('/api/overview/:id', (req, res) => {
+  const { id } = req.params;
   axios
-    .get(`http://localhost:3002/api/hostels/${id}/info`)
+    .get(`http://54.237.214.214/api/overview/${id}`)
     .then(response => res.send(response.data))
-    .catch(err => res.send(`Cannot get /hostels ${err}`));
-})
+    .catch(err => res.send(`Cannot get /api/overview/:id, ${err}`));
+});
+
+app.get('/api/hostels', (req, res) => {
+  axios
+    .get('http://54.237.214.214/api/hostels')
+    .then(response => res.send(response.data))
+    .catch(err => res.send(`Cannot get /api/hostels ${err}`));
+});
+
+app.get('/api/hostels/:id/info', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://54.237.214.214/api/hostels/${id}/info`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(`Cannot get /api/hostels/:id/info ${err}`));
+});
+
+
+//BOOKING
+app.get('/api/hostels/:id/reservations', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://13.57.253.197/api/hostels/${id}/reservations`)
+    .then(response => res.send(response.data))
+    .catch(err => res.json({ message: 'Cannot GET /api/reviews/overview/id' }));
+});
+
+//REVIEWS
+app.get('/api/reviews/:id/all', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://54.172.255.74/api/reviews/${id}/all`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(404).json({ message: 'Cannot GET /api/reviews/id/all' }));
+});
+
+app.get('/api/reviews/overview/:id', (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`http://54.172.255.74/api/reviews/overview/${id}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.json({ message: 'Cannot GET /api/reviews/overview/id' }));
+});
